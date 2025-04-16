242c97dae4b91dd4b8ed0a8520b0f0f242b4f11d
Ian Rich <ianrichb52@outlook.com>
2025-04-16 12:42:38 -0500

    Add to the SafeZone flag and make all flags listeners.

--- src/main/java/net/minebo/kregions/flag/SafeZone.java
+++ src/main/java/net/minebo/kregions/flag/SafeZone.java
@@ -1,9 +1,59 @@
 package net.minebo.kregions.flag;
 
+import net.minebo.kregions.manager.RegionManager;
 import net.minebo.kregions.model.Flag;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class SafeZone extends Flag {
     public SafeZone() {
         super("SafeZone");
     }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if(RegionManager.getRegionByLocation(event.getBlock().getLocation()) != null) {
+            if (RegionManager.getRegionByLocation(event.getBlock().getLocation()).containsFlag(this)) {
+                if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerPlace(BlockPlaceEvent event) {
+        if(RegionManager.getRegionByLocation(event.getBlock().getLocation()) != null) {
+            if (RegionManager.getRegionByLocation(event.getBlock().getLocation()).containsFlag(this)) {
+                if (event.getPlayer().getGameMode() != GameMode.CREATIVE) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onMobSpawn(EntitySpawnEvent event) {
+        if(RegionManager.getRegionByLocation(event.getLocation()) != null) {
+            if (RegionManager.getRegionByLocation(event.getLocation()).containsFlag(this)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onMobBreak(EntityChangeBlockEvent event) {
+        if(RegionManager.getRegionByLocation(event.getBlock().getLocation()) != null) {
+            if (RegionManager.getRegionByLocation(event.getBlock().getLocation()).containsFlag(this)) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
 }
--- src/main/java/net/minebo/kregions/model/Flag.java
+++ src/main/java/net/minebo/kregions/model/Flag.java
@@ -1,13 +1,17 @@
 package net.minebo.kregions.model;
 
+import net.minebo.kregions.KRegions;
 import net.minebo.kregions.manager.FlagManager;
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
 
-public class Flag {
+public class Flag implements Listener {
     public String name;
 
     public Flag(String name) {
         this.name = name;
 
         FlagManager.flags.add(this);
+        Bukkit.getPluginManager().registerEvents(this, KRegions.getInstance());
     }
 }
