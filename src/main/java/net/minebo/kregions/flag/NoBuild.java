a152c0cc0126378e63f3599eef866d3396340e66
kaboomb52 <ianrichb52@outlook.com>
2025-07-02 10:28:15 -0500

    Stop players from editing signs & placing water in NoBuild zones.

--- src/main/java/net/minebo/kregions/flag/NoBuild.java
+++ src/main/java/net/minebo/kregions/flag/NoBuild.java
@@ -3,40 +3,54 @@ package net.minebo.kregions.flag;
 import net.minebo.kregions.manager.RegionManager;
 import net.minebo.kregions.model.Flag;
 import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
 
-public class NoBuild extends Flag {
+public class NoBuild extends Flag implements Listener {
     public NoBuild() {
         super("NoBuild");
     }
 
+    private boolean shouldCancel(Player player, Location location) {
+        return RegionManager.getRegionByLocation(location) != null
+                && RegionManager.getRegionByLocation(location).containsFlag(this)
+                && (player.getGameMode() != GameMode.CREATIVE
+                || !player.isOp()
+                || player.hasMetadata("modmode"));
+    }
+
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        if (RegionManager.getRegionByLocation(event.getBlock().getLocation()) != null &&
-                RegionManager.getRegionByLocation(event.getBlock().getLocation()).containsFlag(this)) {
-
-            if (event.getPlayer().getGameMode() != GameMode.CREATIVE ||
-                    !event.getPlayer().isOp() ||
-                    event.getPlayer().hasMetadata("modmode")) {
-
-                event.setCancelled(true);
-            }
+        if (shouldCancel(event.getPlayer(), event.getBlock().getLocation())) {
+            event.setCancelled(true);
         }
     }
 
     @EventHandler
-    public void onPlayerPlace(BlockPlaceEvent event) {
-        if (RegionManager.getRegionByLocation(event.getBlock().getLocation()) != null &&
-                RegionManager.getRegionByLocation(event.getBlock().getLocation()).containsFlag(this)) {
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (shouldCancel(event.getPlayer(), event.getBlock().getLocation())) {
+            event.setCancelled(true);
+        }
+    }
 
-            if (event.getPlayer().getGameMode() != GameMode.CREATIVE ||
-                    !event.getPlayer().isOp() ||
-                    event.getPlayer().hasMetadata("modmode")) {
+    @EventHandler
+    public void onBucketEmpty(PlayerBucketEmptyEvent event) {
+        // Use getBlockClicked().getLocation() because that's where the fluid is placed
+        if (shouldCancel(event.getPlayer(), event.getBlockClicked().getLocation())) {
+            event.setCancelled(true);
+        }
+    }
 
-                event.setCancelled(true);
-            }
+    @EventHandler
+    public void onSignChange(SignChangeEvent event) {
+        if (shouldCancel(event.getPlayer(), event.getBlock().getLocation())) {
+            event.setCancelled(true);
         }
     }
 }
